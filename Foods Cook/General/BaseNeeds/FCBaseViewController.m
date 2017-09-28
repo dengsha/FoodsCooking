3543ebdf05c85708ce2f756e6c94654ae46877b9
elsa <294676137@qq.com>
2017-09-28 18:00:01 +0800

    add baseviewController
    add baseviewController

---
+++ Foods Cook/General/BaseNeeds/FCBaseViewController.m
@@ -0,0 +1,27 @@
+//
+//  FCBaseViewController.m
+//  Foods Cook
+//
+//  Created by 邓莎 on 2017/9/28.
+//  Copyright © 2017年 邓莎. All rights reserved.
+//
+
+#import "FCBaseViewController.h"
+
+@interface FCBaseViewController ()
+
+@end
+
+@implementation FCBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
