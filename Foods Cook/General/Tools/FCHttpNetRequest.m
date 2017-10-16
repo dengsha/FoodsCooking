3d66cd6e5cc0150bf8dca47e4b2b81c31c3ee967
邓莎 <dengsha@zbj.com>
2017-10-16 17:06:29 +0800

    添加网络请求

---
+++ Foods Cook/General/Tools/FCHttpNetRequest.m
@@ -0,0 +1,21 @@
+//
+//  FCHttpNetRequest.m
+//  Foods Cook
+//
+//  Created by 邓莎 on 2017/10/12.
+//  Copyright © 2017年 邓莎. All rights reserved.
+//
+
+#import "FCHttpNetRequest.h"
+
+@implementation FCHttpNetRequest
+
+- (void)getWithURLString:(NSString *)urlString parameters:(id)parameters success:(SuccessBlock)success failture:(FailtureBlock)failture{
+    
+}
+
+- (void)postWithURLString:(NSString *)urlString parameters:(id)parameters success:(SuccessBlock)success failture:(FailtureBlock)failture{
+    
+}
+
+@end
